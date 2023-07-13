6bca3a3f64506240f78ec8d2727d496fcf61a38c
jmilljr24 <jmilljr24@gmail.com>
2023-07-13 10:13:32 -0400

    Add log out link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   def create
     @post = current_user.posts.build(post_params)
     if @post.save
-      redirect_to @post
+      redirect_to posts_path
     else
       render :new, status: :unprocessable_entity
     end
